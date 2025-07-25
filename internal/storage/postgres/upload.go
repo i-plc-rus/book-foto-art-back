24e23a1f5ffc46802f120889a3862cbb35e672f7
shalimov <shalimov@profiterole.group>
2025-07-25 17:48:37 +0300

    fix table name

--- internal/storage/postgres/upload.go
+++ internal/storage/postgres/upload.go
@@ -13,10 +13,12 @@ type UploadStorage struct {
 
 func (s *Storage) SaveUpload(ctx context.Context, upload model.UploadedPhoto) (*model.UploadedPhoto, error) {
 	row := s.DB.QueryRow(ctx,
-		`INSERT INTO uploaded_photos (collection_id, original_url, thumbnail_url, file_name, file_ext)
-		 VALUES ($1, $2, $3, $4)
-		 RETURNING id`,
-		upload.CollectionID, upload.OriginalURL, upload.ThumbnailURL, upload.FileName, upload.FileExt,
+		`INSERT INTO uploaded_photos 
+    (collection_id, user_id, original_url, thumbnail_url, file_name, file_ext, hash_name)
+     VALUES ($1, $2, $3, $4, $5, $6, $7)
+     RETURNING id`,
+		upload.CollectionID, upload.UserID, upload.OriginalURL, upload.ThumbnailURL,
+		upload.FileName, upload.FileExt, upload.HashName,
 	)
 	var id int64
 	if err := row.Scan(&id); err != nil {

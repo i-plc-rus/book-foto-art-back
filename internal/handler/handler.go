32a2652c30d59db090d8414e4a9e86dbf9d1a6e3
bashkirov <bashkirov@profiterole.group>
2025-08-30 16:12:41 +0400

    swagger doc update

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -514,7 +514,7 @@ func (h *Handler) PublishCollection(c *gin.Context) {
 // RedirectToPublicCollection godoc
 // @Summary      Перенаправление на публичную коллекцию
 // @Description  Перенаправляет на публичную коллекцию по токену
-// @Tags         Collection
+// @Tags         Public
 // @Accept       json
 // @Produce      json
 // @Param        token path string true "Токен короткой ссылки"
@@ -538,7 +538,7 @@ func (h *Handler) RedirectToPublicCollection(c *gin.Context) {
 // GetPublicCollection godoc
 // @Summary      Получить публичную коллекцию
 // @Description  Возвращает публичную коллекцию по токену с возможностью сортировки.
-// @Tags         Collection
+// @Tags         Public
 // @Accept       json
 // @Produce      json
 // @Param        token   path   string true  "Токен короткой ссылки"
